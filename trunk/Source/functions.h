c40168b7a08adc740cedae81e0154cc9236bbb53
Sija <sijawusz@gmail.com>
2005-11-24 14:45:04 +0000

    Initial commit, base for the rest. Configuration is planned more or less, it's not workin' yet though.

---
+++ trunk/Source/functions.h
@@ -0,0 +1,28 @@
+/*
+ *  kAway [shared functions]
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2005 Sijawusz Pur Rahnama
+ *
+ *  $Id: $
+ */
+
+#pragma once
+
+namespace shared {
+  std::string intToStr(int i, int radix = 10) {
+    char buff[64]; _itoa(i, buff, radix);
+    return((std::string)buff);
+  }
+
+  std::string Icon32(int ico) {
+    std::string buff = AP_IMGURL + std::string("reg://IML32/") + shared::intToStr(ico) + ".ICON";
+    return(buff);
+  }
+
+  std::string Icon16(int ico) {
+    std::string buff = "reg://IML16/" + shared::intToStr(ico) + ".ICON";
+    return(buff);
+  }
+}
