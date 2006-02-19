eb9ef16568cb5a60b2f07536205e8ca3623df11f
Sija <sijawusz@gmail.com>
2006-02-19 03:28:06 +0000

    *bump*

---
+++ tags/kAway2_1.0.10.39/Source/Control.h
@@ -0,0 +1,144 @@
+/**
+ *  kAway2 Control class
+ *
+ *  Licensed under The GNU Lesser General Public License
+ *  Redistributions of files must retain the above copyright notice.
+ *
+ *  @filesource
+ *  @copyright    Copyright (c) 2005-2006 Sijawusz Pur Rahnama
+ *  @link         svn://kplugins.net/kaway2/ kAway2 plugin SVN Repo
+ *  @version      $Revision$
+ *  @modifiedby   $LastChangedBy$
+ *  @lastmodified $Date$
+ *  @license      http://creativecommons.org/licenses/LGPL/2.1/
+ */
+
+#pragma once
+
+#include "stdafx.h"
+#include "main.h"
+
+#include "Helpers.h"
+#include "NetList.h"
+#include "Format.h"
+#include "Status.h"
+#include "Message.h"
+
+namespace kAway2 {
+  typedef std::deque<cMessage*> tMsgQueue;
+
+  enum enAutoMsgTpl {
+    tplEnable = cfg::tpl::enable,
+    tplDisable = cfg::tpl::disable,
+    tplReply = cfg::tpl::reply
+  };
+
+  struct sCnt {
+    bool ignored;
+    int historySess;
+    __int64 lastMsgTime;
+    tMsgQueue msgQueue;
+
+    // konstruktor
+    sCnt(bool _ignored = false, int _historySess = 0, int _lastMsgTime = 0) : 
+      ignored(_ignored), historySess(_historySess), lastMsgTime(_lastMsgTime) { }
+  };
+
+  typedef std::map<int, sCnt> tCnts;
+
+  class Control {
+    public:
+      Control();
+      ~Control();
+
+      bool enable(std::string msg = "", int status = 0, bool silent = false);
+      bool disable(std::string msg = "", bool silent = false);
+
+      void sendMsgTpl(int cnt, enAutoMsgTpl tpl, std::string msgVal = "");
+      void showKNotify(const char * text, int ico = ico::logoSmall);
+
+      inline void fromWnd(bool state) {
+        this->isFromWnd = state;
+      }
+
+      inline bool isMuteSwitched() {
+        return(this->muteStateSwitched);
+      }
+
+      inline bool isEnabled() {
+        return(this->isOn);
+      }
+
+      inline bool isAwayMsgSet() {
+        return(this->awayMsg.length() ? true : false);
+      }
+
+      inline bool isAutoAway() {
+        return(this->autoAway);
+      }
+
+      inline void setAutoAway(bool state) {
+        this->autoAway = state;
+      }
+
+      inline void setStatusCtrl(Status *handle) {
+        this->sCtrl = handle;
+      }
+
+      inline int getPluginsGroup() {
+        return(this->pluginsGroup);
+      }
+
+      inline std::string getAwayMsg() {
+        return(this->awayMsg);
+      }
+
+      inline Stamina::Date64 *getAwayTime() {
+        return(this->awayTime);
+      }
+
+      inline tCnts getCnts() {
+        return(this->cntProps);
+      }
+
+      inline sCnt *cntProp(int id) {
+        return(&this->cntProps[Ctrl->DTgetID(DTCNT, id)]);
+      }
+
+      inline void addMsg2CntQueue(int cnt, cMessage *msg) {
+        this->cntProp(cnt)->msgQueue.push_front(messageDuplicate(msg));
+      }
+
+      inline bool isCntSaved(int cnt) {
+        return(this->cntProps.find(cnt) != this->cntProps.end());
+      }
+
+    protected:
+      bool isOn;
+      bool muteStateSwitched;
+      bool autoAway;
+      bool isFromWnd;
+      int pluginsGroup;
+
+      tCnts cntProps;
+      std::string awayMsg;
+      Stamina::Date64 *awayTime;
+
+      void switchBtns(bool state);
+
+    private:
+      Status *sCtrl;
+  };
+
+  namespace lCtrl {
+    NetList *status = NULL;
+
+    NetList *reply = NULL;
+    NetList *sms = NULL;
+    NetList *email = NULL;
+    NetList *forward = NULL;
+  }
+
+  Control *pCtrl = NULL;
+  Status *sCtrl = NULL;
+}
---
+++ tags/kAway2_1.0.10.39/Source/Helpers.cpp
@@ -0,0 +1,195 @@
+/**
+ *  Helpers
+ *
+ *  Licensed under The GNU Lesser General Public License
+ *  Redistributions of files must retain the above copyright notice.
+ *
+ *  @filesource
+ *  @copyright    Copyright (c) 2005-2006 Sijawusz Pur Rahnama
+ *  @link         svn://kplugins.net/kaway2/ kAway2 plugin SVN Repo
+ *  @version      $Revision$
+ *  @modifiedby   $LastChangedBy$
+ *  @lastmodified $Date$
+ *  @license      http://creativecommons.org/licenses/LGPL/2.1/
+ */
+
+#pragma once
+#include "Helpers.h"
+
+/*
+ *  Integer -> String conversion
+ */
+
+std::string itos(int i, int radix) {
+  char buff[64]; _itoa(i, buff, radix);
+  return(buff);
+}
+
+/*
+ *  Bool -> Human readable string
+ */
+
+char * btoa(bool value) {
+  return(value ? "yes" : "no");
+}
+
+std::string btos(bool value) {
+  return(btoa(value));
+}
+
+/*
+ *  NULL -> Human readable string
+ */
+
+char * nullChk(const std::string& value) {
+  return(value.length() ? value.c_str() : "(none)");
+}
+
+char * nullChk(char * value) {
+  return(strlen(value) ? value : "(none)");
+}
+
+/*
+ *  Logging helpers, just a little short cut ;>
+ */
+
+void log(enDebugLevel level, const char * format, va_list ap) {
+  if (Ctrl && Ctrl->DebugLevel(level))
+    Ctrl->IMLOG_(level, format, ap);
+}
+
+void log(const char * format, ...) {
+  va_list ap;
+  va_start(ap, format);
+	log(DBG_LOG, format, ap);
+  va_end(ap);
+}
+
+void logDebug(const char * format, ...) {
+  va_list ap;
+  va_start(ap, format);
+	log(DBG_DEBUG, format, ap);
+  va_end(ap);
+}
+
+/*
+ *  Various helpers
+ */
+
+namespace Helpers {
+  std::string icon16(int ico) {
+    return("reg://IML16/" + itos(ico) + ".ico");
+  }
+
+  std::string icon32(int ico) {
+    return("reg://IML32/" + itos(ico) + ".ico");
+  }
+
+  std::string trim(std::string txt) {
+    CStdString buff(txt); txt = buff.Trim();
+    return(txt);
+  }
+
+  int altCfgVal(int cntId, int colId, bool isBool) {
+    if (isBool)
+      return((GETINT(colId) && (GETCNTI(cntId, colId) < 2) || 
+        (!GETINT(colId) && (GETCNTI(cntId, colId) == 1))) ? true : false);
+    else
+      return((GETCNTI(cntId, colId) >= 0) ? GETCNTI(cntId, colId) : GETINT(colId));
+  }
+
+  const char * altCfgStrVal(int cntId, int colId) {
+    return(strlen(GETCNTC(cntId, colId)) ? GETCNTC(cntId, colId) : GETSTRA(colId));
+  }
+
+  int getPluginsGroup() {
+    return(Ctrl->ICMessage(IMI_GETPLUGINSGROUP));
+  }
+
+  int pluginExists(int net, int type) {
+    return(Ctrl->ICMessage(IMC_FINDPLUG, net, type));
+  }
+
+  const char * getPlugName(int plugID) {
+    return(SAFECHAR((char*) Ctrl->IMessageDirect(IM_PLUG_NAME, plugID)));
+  }
+
+  void UIActionCall(int group, int act, int cntID) {
+    Ctrl->ICMessage(IMI_ACTION_CALL, 
+      (int) &sUIActionNotify_2params(sUIAction(group, act, cntID), ACTN_ACTION, 0, 0), 0);
+  }
+
+  bool isMsgWndOpen(int cntID) {
+    return(Tabs::GetWindowState(cntID));
+  }
+
+  void showKNotify(char * text, int ico) {
+    Ctrl->IMessage(&KNotify::sIMessage_notify(text, ico));
+  }
+
+  int findParentAction(int group, int id) {
+    return(Ctrl->ICMessage(IMI_ACTION_FINDPARENT, (int) &sUIAction(group, id)));
+  }
+
+  int subclassAction(int group, int id, int mask) {
+    sUIActionInfo nfo(group, id);
+    int prevOwner;
+
+    nfo.mask = mask;
+    nfo.txt = new char[100];
+    nfo.txtSize = 99;
+
+    UIActionGet(nfo);
+    if (!(prevOwner = Ctrl->ICMessage(IMI_ACTION_GETOWNER, (int)&nfo.act))) {
+      prevOwner = Ctrl->ICMessage(IMC_PLUG_ID, 0);
+    }
+
+    Ctrl->ICMessage(IMI_ACTION_REMOVE, (int)&nfo.act);
+    Ctrl->ICMessage(IMI_ACTION, (int)&nfo);
+    delete [] nfo.txt;
+
+    return(prevOwner);
+  }
+
+  void clearMru(const char * name) {
+    sMRU mru;
+
+    mru.name = name;
+    mru.count = 0;
+    mru.flags = MRU_GET_USETEMP;
+
+    Ctrl->IMessage(&sIMessage_MRU(IMC_MRU_SET, &mru));
+  }
+
+  void addItemToHistory(cMessage* msg, int cnt, const char * dir, std::string name, int session) {
+    sHISTORYADD item;
+
+    item.cnt = cnt;
+    item.m = msg;
+    item.dir = dir;
+    item.name = name.c_str();
+    item.session = session;
+
+    Ctrl->ICMessage(IMI_HISTORY_ADD, (int) &item);
+  }
+
+  void chgBtn(int group, int id, int cnt, const char * name, int ico, int flags) {
+    sUIActionInfo ai;
+
+    if (name) ai.txt = (char*) name;
+    if (ico) ai.p1 = ico;
+    if (flags >= 0) {
+      ai.status = flags;
+      ai.statusMask = -1;
+    }
+
+    ai.act = sUIAction(group, id, cnt);
+    ai.mask = (name ? UIAIM_TXT : 0) | (ico ? UIAIM_P1 : 0) | (flags >= 0 ? UIAIM_STATUS : 0);
+
+    UIActionSet(ai);
+  }
+
+  void chgBtn(int group, int id, const char * name, int ico, int flags) {
+    UIActionSet(sUIActionInfo(group, id, 0, flags, (char*) name, ico));
+  }
+}
