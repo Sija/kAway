3034db13b9900f9729be379619111d557f74c95a
Sija <sijawusz@gmail.com>
2008-01-22 06:07:31 +0000

    PluginDispatcher okazał się zbędny i został zmerge'owany z PluginController-em :>

--- Source/PluginController.h
+++ Source/PluginController.h
@@ -18,7 +18,15 @@
 #ifndef __PLUGINCONTROLLER_H__
 #define __PLUGINCONTROLLER_H__
 
-#include "PluginDispatcher.h"
+#include "EventDispatcher.h"
+#include "Event.h"
+
+#include "ActionDispatcher.h"
+#include "ActionEvent.h"
+
+#include "IMessageDispatcher.h"
+#include "IMEvent.h"
+
 #include "Config.h"
 
 using namespace Stamina;
@@ -26,12 +34,12 @@ using namespace boost;
 
 namespace Konnekt {
   template <class T>
-  class PluginController : public PluginDispatcher {
+  class PluginController : public SharedObject<iSharedObject>, public signals::trackable {
   public:
     /**
      * Class version macro
      */
-    STAMINA_OBJECT_CLASS_VERSION(PluginController<T>, PluginDispatcher, Version(0,1,0,0));
+    STAMINA_OBJECT_CLASS_VERSION(PluginController<T>, iSharedObject, Version(0,3,0,0));
 
   public:
     typedef SharedPtr<T> oInstance;
@@ -41,6 +49,14 @@ namespace Konnekt {
      * Creates new PluginController object.
      */
     inline PluginController() {
+      // setting/unsetting Ctrl global pointer
+      _dispatcher.connect(IM_PLUG_INIT, bind(&PluginController::_onPlugInit, this, _1));
+      _dispatcher.connect(IM_PLUG_DEINIT, bind(&PluginController::_onPlugDeInit, this, _1));
+
+      // actions subclassing
+      _dispatcher.connect(IM_UI_PREPARE, bind(&ActionDispatcher::doSubclass, &_action_dispatcher, _1));
+
+      // initialize configuration
       config = new Config(getIMessageDispatcher());
     }
 
@@ -66,7 +82,70 @@ namespace Konnekt {
       return getInstance()->config;
     }
 
+  public:
+    /**
+     * Dispatch incoming IMessage
+     *
+     * @param sIMessage_base*
+     */
+    inline int dispatch(sIMessage_base* msgBase) {
+      // dispatch IMessage
+      oEvent ev = _dispatcher.dispatch(msgBase);
+
+      // dispatch action
+      if (ev->getID() == IM_UIACTION) {
+        ev = _action_dispatcher.dispatch(msgBase);
+      }
+      return ev->getReturnValue();
+    }
+
+    /**
+     * Returns reference to IMessageDispatcher
+     *
+     * @return IMessageDispatcher&
+     */
+    inline IMessageDispatcher& getIMessageDispatcher() {
+      return _dispatcher;
+    }
+
+    /**
+     * Returns reference to IMActionDispatcher
+     *
+     * @return IMActionDispatcher&
+     */
+    inline ActionDispatcher& getActionDispatcher() {
+      return _action_dispatcher;
+    }
+
   protected:
+    /**
+     * Plugin initialization
+     *
+     * @param IMEvent&
+     */
+    inline void _onPlugInit(IMEvent& ev) {
+      sIMessage* im = ev.getIMessage();
+      Plug_Init(im->p1, im->p2);
+
+      ev.setSuccess();
+    }
+
+    /**
+     * Plugin deinitialization
+     *
+     * @param IMEvent&
+     */
+    inline void _onPlugDeInit(IMEvent& ev) {
+      sIMessage* im = ev.getIMessage();
+      Plug_Deinit(im->p1, im->p2);
+
+      ev.setSuccess();
+    }
+
+  protected:
+    IMessageDispatcher _dispatcher;
+    ActionDispatcher _action_dispatcher;
+
     static oInstance instance;
     oConfig config;
   };
